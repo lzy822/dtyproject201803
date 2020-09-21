dbec5fa82bb43f8a3193b0cb247374b9d3d34630
Zhengyang <542865759@qq.com>
2020-09-21 15:42:50 +0800

    更新版本信息和文件目录结构

--- app/src/main/java/com/esri/arcgisruntime/demos/displaymap/StaticVariableEnum.java
+++ app/src/main/java/com/esri/arcgisruntime/demos/displaymap/StaticVariableEnum.java
@@ -13,11 +13,11 @@ public class StaticVariableEnum {
     public static final int PERMISSION_CODE_2 = 42044;
     public static final String JBNTGDBROOTPATH = Environment.getExternalStorageDirectory().toString() + "/临沧市基本农田/基本农田保护区.geodatabase";
     public static final String GDBROOTPATH = Environment.getExternalStorageDirectory().toString() + "/临沧市基本农田/临沧市5309省标准乡级土地利用总体规划及基本农田数据库2000.geodatabase";
-    public static final String MMPKROOTPATH = Environment.getExternalStorageDirectory().toString() + "/临沧市基本农田/临沧市土地利用规划和基本农田数据.mmpk";
+    public static final String MMPKROOTPATH = Environment.getExternalStorageDirectory().toString() + "/临沧市基本农田/临沧市级外业踏勘数据.mmpk";
     public static final String PGDBROOTPATH = Environment.getExternalStorageDirectory().toString() + "/临沧市基本农田/临沧市地类图斑p图斑数据库.geodatabase";
     public static final String XZCAreaROOTPATH = Environment.getExternalStorageDirectory().toString() + "/临沧市基本农田/1229村委会界线/行政区_村委会.shp";
     public static final String KSTestROOTPATH = Environment.getExternalStorageDirectory().toString() + "/20190102矿山测试数据/临沧市砂石料粘土矿点数据.geodatabase";
     public static final String DLLCROOTPATH = Environment.getExternalStorageDirectory().toString() + "/临沧市基本农田/临沧大理过程稿.geodatabase";
-    public static final String LCDKYROOTPATH = Environment.getExternalStorageDirectory().toString() + "/临沧市基本农田/临沧市级外业踏勘数据.geodatabase";
+    public static final String LCDKYROOTPATH = Environment.getExternalStorageDirectory().toString() + "/临沧外业踏勘系统/临沧市级外业踏勘数据.geodatabase";
 
 }
